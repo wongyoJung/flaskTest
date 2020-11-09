9463c6b452ec58f6749150598335a0e43a989365
wongyoJung <kbb329@gmail.com>
2020-11-10 05:56:24 +0900

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
@@ -8,8 +10,11 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def sessions():
-    return render_template('session.html')
+    return render_template('index.html')
 
+@app.route('/view')
+def view():
+    return render_template('view.html')
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
@@ -21,5 +26,21 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 
+@socketio.on('stream')
+def handle_message(message):
+    print('received message: ' + message)
+    socketio.emit('received', message, callback=messageReceived)
+
+@socketio.on('streamVideo')
+def handle_message_stream(message):
+    print('received message: ')
+    socketio.emit('receivedStream', message, callback=messageReceived)
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+# if __name__ == "__main__":
+#     app.run(ssl_context='adhoc')
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True, keyfile='key.pem', certfile='cert.pem')
